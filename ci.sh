7b4631448679fcbbc96ea2f7d5e0c0b522249ac2
Evgeniy Abramov <i@kio.ninja>
2025-05-17 17:00:36 +0300

    fix: types for messages and make ci is better

--- ci.sh
+++ ci.sh
@@ -3,15 +3,15 @@
 set -e
 
 # Build
-cargo build
+cargo build -p hyperliquid_rust_sdk
 
 # Check formatting
 cargo fmt -- --check
 
 # Run Clippy
-cargo clippy -- -D warnings
+cargo clippy -p hyperliquid_rust_sdk -- -D warnings
 
 # Run tests
-cargo test
+cargo test -p hyperliquid_rust_sdk
 
 echo "CI checks passed successfully."
