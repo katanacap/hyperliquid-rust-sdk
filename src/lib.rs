e0d848fe40f1b4ae378d4602c9f42adb732028c3
Evgeniy Abramov <i@kio.ninja>
2025-05-20 08:09:59 +0900

    chore: better public interfaces

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,12 @@ mod proxy_digest;
 mod req;
 mod signature;
 mod ws;
+
 pub use consts::{EPSILON, LOCAL_API_URL, MAINNET_API_URL, TESTNET_API_URL};
 pub use errors::Error;
 pub use exchange::*;
 pub use helpers::{bps_diff, truncate_float, BaseUrl};
 pub use info::{info_client::*, *};
 pub use market_maker::{MarketMaker, MarketMakerInput, MarketMakerRestingOrder};
-pub use meta::{AssetMeta, Meta};
+pub use meta::{AssetMeta, Meta, SpotAssetContext, SpotMeta, SpotMetaAndAssetCtxs};
 pub use ws::*;
